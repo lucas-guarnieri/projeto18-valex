52b944593ee053398790d327d5931e1e5c2b12fc
lucas-guarnieri <lucaslguarnieri@gmail.com>
2022-07-10 21:16:41 -0300

    feat - company api verification

---
+++ src/middlewares/apiKeyVerificationMiddleware.ts
@@ -0,0 +1,23 @@
+import { NextFunction, Request, Response } from "express";
+import { findByApiKey } from "../repositories/companyRepository.js"
+
+export async function apiKeyValidation(req: Request, res: Response, next: NextFunction ) {
+    const apiKey = req.header("x-api-key");
+    
+    if (!apiKey) {
+        throw {
+            type: "apiValidationError", message: "valid apiKey missing", code: "400"
+        }
+    }
+
+    const company  = await findByApiKey(apiKey);
+    if (!company) {
+        throw {
+            type: "apiValidationError", message: "invalid api", code: "401"
+        }
+    }
+
+    res.locals.companyId = {companyId: company.id }
+
+    next()
+}
--- src/routers/cardRoute.ts
+++ src/routers/cardRoute.ts
@@ -3,10 +3,10 @@ import { Router } from "express";
 
 const cardRoute = Router();
 cardRoute.post("/card/create")
-cardRoute.post("/card/activate/:cardId")
-cardRoute.get("card/balance/:cardId")//pega balanço de cartão do usuário
-cardRoute.get("card/:cardId")//pega todos os cartões do usuário
-cardRoute.post("card/block/:cardId")
-cardRoute.post("card/unblock/:cardId")
+// cardRoute.post("/card/activate/:cardId")
+// cardRoute.get("card/balance/:cardId")//pega balanço de cartão do usuário
+// cardRoute.get("card/:cardId")//pega todos os cartões do usuário
+// cardRoute.post("card/block/:cardId")
+// cardRoute.post("card/unblock/:cardId")
 
 export default cardRoute;
